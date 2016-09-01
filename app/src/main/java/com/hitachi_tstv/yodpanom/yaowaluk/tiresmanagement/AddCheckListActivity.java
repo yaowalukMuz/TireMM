1fb0eccd3e0617971b8b3d598eafc1f25a27dfc6
yaowamuz <yaowamuz.c@gmail.com>
2016-09-01 16:08:17 +0700

    Tire_Serial successfully_18Aug16  Addition Spinner for reason

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/tiresmanagement/AddCheckListActivity.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/tiresmanagement/AddCheckListActivity.java
@@ -1,13 +1,17 @@
 package com.hitachi_tstv.yodpanom.yaowaluk.tiresmanagement;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +23,9 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -26,15 +33,17 @@ public class AddCheckListActivity extends AppCompatActivity {
 
     private TextView tireIdTextView;
     private EditText odoEditText,deepEditText,presureEditText,commentEditText ;
-    private String tireIdString,serialString,odoString,deepString,presureString,commentString,dateString,url,username;
+    private String tireIdString,serialString,odoString,deepString,presureString,commentString,
+            dateString,url,username,urlReason,reasonIdString,reasonNameString;
     private DatePicker checkDatePicker;
-
+    private Spinner reason1Spinner;
 
     static final int  DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_add_check_list);
 
         ConstantUrl constantUrl = new ConstantUrl();
@@ -45,6 +54,7 @@ public class AddCheckListActivity extends AppCompatActivity {
         presureEditText = (EditText) findViewById(R.id.editText4);
         commentEditText = (EditText) findViewById(R.id.editText7);
         checkDatePicker = (DatePicker) findViewById(R.id.datePicker);
+        reason1Spinner = (Spinner) findViewById(R.id.spinner);
 
         setCurrentDateOnView();
 
@@ -52,13 +62,73 @@ public class AddCheckListActivity extends AppCompatActivity {
         serialString = getIntent().getStringExtra("Serial");
         username = getIntent().getStringExtra("username");
 
-        tireIdTextView.setText("Tire Series ::" + serialString);
+       // tireIdTextView.setText("Tire Series ::" + serialString);
 
 
         url = constantUrl.getUrlAddCheckList();
+        urlReason = constantUrl.getUrlJSONReason();
+
+        SyncReason syncReason = new SyncReason(reason1Spinner, this, urlReason);
+        syncReason.execute();
 
 
     }//main method
+
+
+    private class SyncReason extends AsyncTask<Void, Void, String> {
+
+    private Spinner mySpinner;
+    private Context context;
+    private String myURL;
+    private String[] reasonStrings,idStrings;
+
+        public SyncReason(Spinner mySpinner, Context context, String myURL) {
+            this.mySpinner = mySpinner;
+            this.context = context;
+            this.myURL = myURL;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(myURL).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+
+                Log.d("Spinner", "e doInBack ==> " + e.toString());
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("Spinner", "e onPostExecute---->" +s);
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+                reasonStrings = new String[jsonArray.length()];
+                idStrings = new String[jsonArray.length()];
+
+                for(int i = 0;i < jsonArray.length();i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    reasonStrings[i] = jsonObject.getString("res_name");
+                    idStrings[i] =  jsonObject.getString("res_id");
+                }
+
+                ArrayAdapter arrayAdapter = new ArrayAdapter(context, R.layout.support_simple_spinner_dropdown_item, reasonStrings);
+                reason1Spinner.setAdapter(arrayAdapter);
+
+            } catch (Exception e) {
+                Log.d("Spinner", "e onPostExecute+++" + e.toString());
+            }
+        }
+    }
+
 
 
     public void setCurrentDateOnView() {
@@ -80,6 +150,7 @@ public class AddCheckListActivity extends AppCompatActivity {
         commentString = commentEditText.getText().toString().trim();
         int month = checkDatePicker.getMonth() + 1;
         dateString = checkDatePicker.getYear() + "-" + month + "-" + checkDatePicker.getDayOfMonth();
+        //reasonIdString = reason1Spinner.get
         Log.d("Date", dateString);
 
 
--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/tiresmanagement/ConstantUrl.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/tiresmanagement/ConstantUrl.java
@@ -5,10 +5,11 @@ package com.hitachi_tstv.yodpanom.yaowaluk.tiresmanagement;
  */
 public class ConstantUrl {
 
-    private String urlJSONuser = "http://service.eternity.co.th/tires/system/CenterService/getUser.php";
-    private String urlJSONLicense = "http://service.eternity.co.th/tires/system/CenterService/getVehicle.php";
-    private String urlJSONFormatWhell = "http://service.eternity.co.th/tires/system/CenterService/getFormatVehicle.php";
-    private String urlAddCheckList = "http://service.eternity.co.th/tires/system/CenterService/addCheckList.php";
+    private String urlJSONuser = "http://service.eternity.co.th/tires_test/system/CenterService/getUser.php";
+    private String urlJSONLicense = "http://service.eternity.co.th/tires_test/system/CenterService/getVehicle.php";
+    private String urlJSONFormatWhell = "http://service.eternity.co.th/tires_test/system/CenterService/getFormatVehicle.php";
+    private String urlAddCheckList = "http://service.eternity.co.th/tires_test/system/CenterService/addCheckList.php";
+    private String urlJSONReason = "http://service.eternity.co.th/tires_test/system/CenterService/getReason.php";
 
     public String getUrlJSONuser() {
         return urlJSONuser;
@@ -25,4 +26,9 @@ public class ConstantUrl {
     public String getUrlJSONFormatWhell() {
         return urlJSONFormatWhell;
     }
+
+    public String getUrlJSONReason() {
+        return  urlJSONReason;
+    }
+
 }
